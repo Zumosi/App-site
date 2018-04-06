31bf0f384d71d4f2ad2da6a5d5994c9217e2bf09
Franck <n-franck@hotmail.fr>
2018-04-06 14:44:06 +0200

    BD

--- mvc.sql
+++ mvc.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  ven. 06 avr. 2018 à 12:37
+-- Généré le :  ven. 06 avr. 2018 à 12:43
 -- Version du serveur :  5.7.19
 -- Version de PHP :  7.1.9
 
@@ -21,6 +21,233 @@ SET time_zone = "+00:00";
 --
 -- Base de données :  `athome`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `actionneur`
+--
+
+DROP TABLE IF EXISTS `actionneur`;
+CREATE TABLE IF NOT EXISTS `actionneur` (
+  `id_actionneur` int(11) NOT NULL AUTO_INCREMENT,
+  `etat` varchar(255) NOT NULL,
+  `type` varchar(60) CHARACTER SET latin1 COLLATE latin1_bin NOT NULL,
+  `id_capteur` int(11) NOT NULL,
+  PRIMARY KEY (`id_actionneur`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `actionneur`
+--
+
+INSERT INTO `actionneur` (`id_actionneur`, `etat`, `type`, `id_capteur`) VALUES
+(1, 'marche', 'moteur', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `boutique`
+--
+
+DROP TABLE IF EXISTS `boutique`;
+CREATE TABLE IF NOT EXISTS `boutique` (
+  `id_boutique` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(40) CHARACTER SET latin1 COLLATE latin1_bin NOT NULL,
+  `description` text CHARACTER SET latin1 COLLATE latin1_bin NOT NULL,
+  `prix` decimal(10,2) NOT NULL,
+  `id_personne` int(11) NOT NULL,
+  `stock` int(2) NOT NULL,
+  PRIMARY KEY (`id_boutique`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `boutique`
+--
+
+INSERT INTO `boutique` (`id_boutique`, `nom`, `description`, `prix`, `id_personne`, `stock`) VALUES
+(1, 'infrarouge', 'Haute densité d\'assemblage par intégration de la diode réceptrice, du filtre et du préampli dans un boîtier. Insensibilité à la lumière du jour, compatibilité TTL et C-MOS, faible puissance absorbée et haute sécurité anti-parasite distinguent cette série.', '0.65', 1, 5);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `capteur`
+--
+
+DROP TABLE IF EXISTS `capteur`;
+CREATE TABLE IF NOT EXISTS `capteur` (
+  `id_capteur` int(11) NOT NULL AUTO_INCREMENT,
+  `type` varchar(255) NOT NULL,
+  `reference` varchar(255) NOT NULL,
+  `etat` varchar(255) NOT NULL,
+  `id_place` int(11) NOT NULL,
+  PRIMARY KEY (`id_capteur`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `capteur`
+--
+
+INSERT INTO `capteur` (`id_capteur`, `type`, `reference`, `etat`, `id_place`) VALUES
+(1, 'infrarouge', 'LHI 968', 'marche', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `facture`
+--
+
+DROP TABLE IF EXISTS `facture`;
+CREATE TABLE IF NOT EXISTS `facture` (
+  `id_facture` int(11) NOT NULL AUTO_INCREMENT,
+  `nom_utilisateur` varchar(200) CHARACTER SET latin1 COLLATE latin1_bin NOT NULL,
+  `nom_produit` varchar(200) CHARACTER SET latin1 COLLATE latin1_bin NOT NULL,
+  `prix` decimal(10,0) NOT NULL,
+  `num_adresse` int(4) NOT NULL,
+  `rue_adresse` varchar(20) NOT NULL,
+  `nom_adresse` varchar(20) CHARACTER SET latin1 COLLATE latin1_bin NOT NULL,
+  `code_postal` int(5) NOT NULL,
+  PRIMARY KEY (`id_facture`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `facture`
+--
+
+INSERT INTO `facture` (`id_facture`, `nom_utilisateur`, `nom_produit`, `prix`, `num_adresse`, `rue_adresse`, `nom_adresse`, `code_postal`) VALUES
+(1, 'Nguyen', 'Modules récepteurs à infrarouge TSOP4838 38 kHz - TSOP 4838', '1', 10, 'Avenue de', 'Paris', 94300);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `faq`
+--
+
+DROP TABLE IF EXISTS `faq`;
+CREATE TABLE IF NOT EXISTS `faq` (
+  `id_faq` int(11) NOT NULL,
+  `titre` varchar(255) CHARACTER SET latin1 COLLATE latin1_bin NOT NULL,
+  `reponse` text CHARACTER SET latin1 COLLATE latin1_bin NOT NULL,
+  `auteur` varchar(50) CHARACTER SET latin1 COLLATE latin1_bin NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `faq`
+--
+
+INSERT INTO `faq` (`id_faq`, `titre`, `reponse`, `auteur`) VALUES
+(1, 'Mon capteur marche pas', 'Veuillez à vérifier les connexion.\r\nRegardez sur le site si le capteur marche toujours. \r\nAprès ces modifications si cela ne marche pas nous contacter à l\'email suivant : athomeequipe@gmail.com', 'Franck Nguyen (Admin)');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `habitation`
+--
+
+DROP TABLE IF EXISTS `habitation`;
+CREATE TABLE IF NOT EXISTS `habitation` (
+  `id_habitation` int(11) NOT NULL AUTO_INCREMENT,
+  `type` varchar(40) CHARACTER SET latin1 COLLATE latin1_bin NOT NULL,
+  `id_user` int(11) NOT NULL,
+  PRIMARY KEY (`id_habitation`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `habitation`
+--
+
+INSERT INTO `habitation` (`id_habitation`, `type`, `id_user`) VALUES
+(1, 'Appartement', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `message`
+--
+
+DROP TABLE IF EXISTS `message`;
+CREATE TABLE IF NOT EXISTS `message` (
+  `id_message` int(11) NOT NULL AUTO_INCREMENT,
+  `id_topic` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `commentaire` text CHARACTER SET latin1 COLLATE latin1_bin NOT NULL,
+  `date_commentaire` datetime NOT NULL,
+  PRIMARY KEY (`id_message`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `message`
+--
+
+INSERT INTO `message` (`id_message`, `id_topic`, `id_user`, `commentaire`, `date_commentaire`) VALUES
+(1, 1, 1, 'Voici les règles d\'usage de ce forum.', '2018-04-06 14:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `piece`
+--
+
+DROP TABLE IF EXISTS `piece`;
+CREATE TABLE IF NOT EXISTS `piece` (
+  `id_piece` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(40) CHARACTER SET latin1 COLLATE latin1_bin NOT NULL,
+  `superficie` tinyint(3) NOT NULL,
+  `id_maison` int(11) NOT NULL,
+  `type` varchar(40) NOT NULL,
+  PRIMARY KEY (`id_piece`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `piece`
+--
+
+INSERT INTO `piece` (`id_piece`, `nom`, `superficie`, `id_maison`, `type`) VALUES
+(1, 'Chambre A', 15, 1, 'Chambre');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `statistique`
+--
+
+DROP TABLE IF EXISTS `statistique`;
+CREATE TABLE IF NOT EXISTS `statistique` (
+  `id_stat` int(11) NOT NULL AUTO_INCREMENT,
+  `id_sensor` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `donnee` text NOT NULL,
+  PRIMARY KEY (`id_stat`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `statistique`
+--
+
+INSERT INTO `statistique` (`id_stat`, `id_sensor`, `date`, `donnee`) VALUES
+(1, 1, '2018-04-06', 'Puissance 10 mW\r\nTempérature 10 °C');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `topic`
+--
+
+DROP TABLE IF EXISTS `topic`;
+CREATE TABLE IF NOT EXISTS `topic` (
+  `id_topic` int(11) NOT NULL AUTO_INCREMENT,
+  `titre` varchar(40) CHARACTER SET latin1 COLLATE latin1_bin NOT NULL,
+  `id_utilisateur` int(11) NOT NULL,
+  `date_crea` datetime NOT NULL,
+  PRIMARY KEY (`id_topic`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `topic`
+--
+
+INSERT INTO `topic` (`id_topic`, `titre`, `id_utilisateur`, `date_crea`) VALUES
+(1, 'Regle du Forum', 1, '2018-04-06 14:00:00');
 
 -- --------------------------------------------------------
 
