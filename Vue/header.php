90b025cd802d52667691424ff44dc273c7fd005d
cleman95 <34369901+cleman95@users.noreply.github.com>
2018-04-06 14:47:39 +0200

    Add files via upload
    Header avec menu deroulant

---
+++ Vue/header.php
@@ -0,0 +1,51 @@
+<?php
+/**
+* Vue : Header HTML
+*/
+?>
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8" />
+  <link rel="stylesheet" href="header.css" />
+  </head>
+<body>
+	<div class="headerfixe">
+	<header>
+	
+		<img src="bandeHaut.jpg" id="bande"/>
+		<a href="index"><img src="AtHomeTransparent.png" id="logo" /></a>
+		
+<nav>
+		<ul id="menu">
+	<li><a href="#">Ma Maison</a>
+		<ul>
+			<li><a href="#">Plan</a></li>
+			<li><a href="#">capteurs</a></li>
+			<li><a href="#">statistiques</a></li>
+		</ul>
+	</li>
+	<li><a href="#">Magasin</a>
+		<ul>
+			<li><a href="#">Capteurs</a></li>
+			<li><a href="#">factures</a></li>
+		</ul>
+	</li>
+	<li><a href="#">Mon Compte</a>
+		<ul>
+			<li><a href="#">profil</a></li>
+			<li><a href="#">Gestion capteurs</a></li>
+			<li><a href="#">Gestion utilisateurs</a></li>
+			<li><a href="#">Deconnexion</a></li>
+		</ul>
+	</li>
+	<li><a href="#">Contactez Nous</a>
+		<ul>
+			<li><a href="#">Forum</a></li>
+			<li><a href="#">FAQ</a></li>
+		</ul>
+	</li>
+</ul>
+	</div>
+</nav>
+</header>
