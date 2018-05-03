166b938f4f50144deefafe4ec8feb7209f9b5886
Zumosi <32439520+Zumosi@users.noreply.github.com>
2018-05-03 11:56:18 +0200

    Update index.php
    Update index

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
 <?php
 include ("Vue/header.php");
+
+include("Modèle/requete.boutique.php");
+
+if(isset($_GET['cible']) && !empty($_GET['cible'])) {
+    $url = $_GET['cible'];
+}
+else {
+    $url='header';
+}
+
+
+include ('Vue/' .$url.'.php');
+
+include ("Vue/footer.html");
+
 ?>
