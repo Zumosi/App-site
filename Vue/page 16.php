f7ee575bd98f416649a626aee40caed98492be68
ValentinPICONE <val.picone@sfr.fr>
2018-05-04 16:04:02 +0200

    ajout page 18complet

--- Vue/page 16.php
+++ Vue/page 16.php
@@ -5,59 +5,24 @@
     <meta charset="UTF-8">
     <title>page 16</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
-
-
-    <style>
-        img {
-            width: 10vw;
-            height: auto;
-        }
-    </style>
-    <style>
-        caption
-        {
-            font-size: 5vw;
-            text-align: center;
-            caption-side: top;
-        }
-        td
-        {
-            border: none;
-            text-align: center;
-            background-color: transparent;
-        }
-        table
-        {
-            margin: 0 auto;
-            margin-bottom: 3vw;
-            border-spacing: 8vw 1vw;
-            background-color: transparent;
-        }
-
-    </style>
-
-
-
-
-
-
+    <link rel="stylesheet" href="page%2016.css" />
 
 </head>
 <body>
-
+<?php include ("header.php")
+?>
 
 <table>
     <caption> Les capteurs </caption>
     <tr>
-        <td><p>Luminositée</td>
-        <td><p>Humiditée</p></td>
+        <td><p>Luminosité</td>
+        <td><p>Humidité</p></td>
         <td><p>Infra-rouge</p></td>
     </tr>
     <tr>
-        <td><p><a href="index.php?cible=page17" title="Luminositée"><img src="Vue/image/brightness.png"></a></p></td>
-        <td><p><a href="pagehumidité.html" title="Humiditée"><img src="Vue/image/humidity.png"></a></p></td>
-        <td><p><a href="pageinfrarouge.html" title="Infra-rouge"><img src="Vue/image/infrared.png"></a></p></td>
+        <td><p><a href="page17.php?capteur=lux" title="Luminosité"><img src="image/brightness.png"></a></p></td>
+        <td><p><a href="page17.php?capteur=hum" title="Humidité"><img src="image/humidity.png"></a></p></td>
+        <td><p><a href="page17.php?capteur=ir" title="Infra-rouge"><img src="image/infrared.png"></a></p></td>
     </tr>
     <tr>
         <td><p>Micro</td>
@@ -65,13 +30,14 @@
         <td><p>Temperature</p></td>
     </tr>
     <tr>
-        <td><p><a href="pagemicro.html" title="Micro"><img src="Vue/image/micro.png"></a></p></td>
-        <td><p><a href="pagevideocamera.html" title="Video-camera"><img src="Vue/image/video-camera.png"></a></p></td>
-        <td><p><a href="pagetemperature.html" title="Temperature"><img src="Vue/image/temperature.png"></a></p></td>
+        <td><p><a href="page17.php?capteur=mic" title="Micro"><img src="image/micro.png"></a></p></td>
+        <td><p><a href="page17.php?capteur=vid" title="Video-camera"><img src="image/video-camera.png"></a></p></td>
+        <td><p><a href="page17.php?capteur=temp" title="Temperature"><img src="image/temperature.png"></a></p></td>
     </tr>
 </table>
 
-
+<?php include ("footer.html");
+?>
 
 </body>
 </html>
--- Vue/page17.php
+++ Vue/page17.php
@@ -1,135 +1,276 @@
-<?php
-?>
+
+
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <title>page 17</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <style>
-        caption
-        {
-            font-size: 5vw;
-            text-align: center;
-            caption-side: top;
-        }
-        table
-        {
-            border: 1px solid black;
-            border-radius: 1vw;
-            width: 70vw;
-            border-spacing: 8vw 1vw;
-            margin-left: auto;
-            margin-right: auto;
-        }
-        th
-        {
-            text-align: center;
-            font-weight: bold;
-            font-size: 1.5vw;
-        }
-        td
-        {
-            border: 1px solid black;
-            text-align: center;
-            border-radius: 10px;
-        }
-        a
-        {
-            text-decoration: none;
-            font-weight: bold;
-        }
-        a:visited
-        {
-            color: black;
-        }
-        .onoffswitch {
-            position: relative; width: 90px;
-            -webkit-user-select:none; -moz-user-select:none; -ms-user-select: none;
-        }
-        .onoffswitch-checkbox {
-            display: none;
-        }
-        .onoffswitch-label {
-            display: block; overflow: hidden; cursor: pointer;
-            border: 2px solid darkgrey; border-radius: 20px;
-        }
-        .onoffswitch-inner {
-            display: block; width: 200%; margin-left: -100%;
-            transition: margin 0.3s ease-in 0s;
-        }
-        .onoffswitch-inner:before, .onoffswitch-inner:after {
-            display: block; float: left; width: 50%; height: 30px; padding: 0; line-height: 30px;
-            font-size: 14px; color: white; font-family: Trebuchet, Arial, sans-serif; font-weight: bold;
-            box-sizing: border-box;
-        }
-        .onoffswitch-inner:before {
-            content: "ON";
-            padding-left: 10px;
-            background-color: green; color: white;
-            text-align: left;
-        }
-        .onoffswitch-inner:after {
-            content: "OFF";
-            padding-right: 10px;
-            background-color: red; color: white;
-            text-align: right;
-        }
-        .onoffswitch-switch {
-            display: block; width: 18px; margin: 6px;
-            background: white;
-            position: absolute; top: 0; bottom: 0;
-            right: 56px;
-            border: 2px solid darkgrey; border-radius: 20px;
-            transition: all 0.3s ease-in 0s;
-        }
-        .onoffswitch-checkbox:checked + .onoffswitch-label .onoffswitch-inner {
-            margin-left: 0;
-        }
-        .onoffswitch-checkbox:checked + .onoffswitch-label .onoffswitch-switch {
-            right: 0px;
-        }
-        </style>
-        </head>
+
+    <link rel="stylesheet" href="page%2017.css" />
+</head>
 <body>
+<?php include ("header.php")
+?>
+<?php if ($_GET['capteur']== 'lux') {
+    echo
+    '<table >
+    <caption > Luminosité </caption >
+    <tr >
+        <th >
+    IdCapteur:
+        </th >
+        <th >
+    Admin:
+        </th >
+        <th >
+            <div class="onoffswitch" >
+                <input type = "checkbox" name = "onoffswitch" class="onoffswitch-checkbox" id = "myonoffswitch" checked >
+                <label class="onoffswitch-label" for="myonoffswitch" >
+                    <span class="onoffswitch-inner" ></span >
+                    <span class="onoffswitch-switch" ></span >
+                </label >
+            </div >
 
+        </th >
+        <td >
+            <a href = "pagemodifier-ajouter.html" > Modifier</a >
+        </td >
+    </tr >
+    <tr >
+        <th >
+    Place:
+        </th >
+        <th >
+    Etat:
+        </th >
 
-<table>
-    <caption> Luminositée </caption>
-    <tr>
-        <th>
-            IdCapteur:
-        </th>
-        <th>
-            Admin:
-        </th>
-        <th>
-            <div class="onoffswitch">
-                <input type="checkbox" name="onoffswitch" class="onoffswitch-checkbox" id="myonoffswitch" checked>
-                <label class="onoffswitch-label" for="myonoffswitch">
-                    <span class="onoffswitch-inner"></span>
-                    <span class="onoffswitch-switch"></span>
-                </label>
-            </div>
+    </tr >
+</table >
+<table style = "margin-top: 2vw" >
+    <tr >
+        <td ><a href = "pagemodifier-ajouter.html" > +</a ></td >
+        <th style = "text-align: left" > Ajouter un capteur </th >
+    </tr >
+</table >
+';
+}
+elseif ($_GET['capteur']== 'hum') {
+    echo
+    '<table >
+    <caption > Humidité </caption >
+    <tr >
+        <th >
+    IdCapteur:
+        </th >
+        <th >
+    Admin:
+        </th >
+        <th >
+            <div class="onoffswitch" >
+                <input type = "checkbox" name = "onoffswitch" class="onoffswitch-checkbox" id = "myonoffswitch" checked >
+                <label class="onoffswitch-label" for="myonoffswitch" >
+                    <span class="onoffswitch-inner" ></span >
+                    <span class="onoffswitch-switch" ></span >
+                </label >
+            </div >
 
-        </th>
-        <td>
-            <a href="pagemodifier-ajouter.html">Modifier</a>
-        </td>
-    </tr>
-    <tr>
-        <th>
-            Place:
-        </th>
-        <th>
-            Etat:
-        </th>
+        </th >
+        <td >
+            <a href = "pagemodifier-ajouter.html" > Modifier</a >
+        </td >
+    </tr >
+    <tr >
+        <th >
+    Place:
+        </th >
+        <th >
+    Etat:
+        </th >
 
-    </tr>
-</table>
-<table style="margin-top: 2vw">
-    <tr>
-        <td><a href="pagemodifier-ajouter.html">+</a></td>
-        <th style="text-align: left">Ajouter un capteur</th>
-    </tr>
-</table>
+    </tr >
+</table >
+<table style = "margin-top: 2vw" >
+    <tr >
+        <td ><a href = "pagemodifier-ajouter.html" > +</a ></td >
+        <th style = "text-align: left" > Ajouter un capteur </th >
+    </tr >
+</table >
+';
+}
+elseif ($_GET['capteur']== 'ir') {
+    echo
+    '<table >
+    <caption > Infra-rouge </caption >
+    <tr >
+        <th >
+    IdCapteur:
+        </th >
+        <th >
+    Admin:
+        </th >
+        <th >
+            <div class="onoffswitch" >
+                <input type = "checkbox" name = "onoffswitch" class="onoffswitch-checkbox" id = "myonoffswitch" checked >
+                <label class="onoffswitch-label" for="myonoffswitch" >
+                    <span class="onoffswitch-inner" ></span >
+                    <span class="onoffswitch-switch" ></span >
+                </label >
+            </div >
+
+        </th >
+        <td >
+            <a href = "pagemodifier-ajouter.html" > Modifier</a >
+        </td >
+    </tr >
+    <tr >
+        <th >
+    Place:
+        </th >
+        <th >
+    Etat:
+        </th >
+
+    </tr >
+</table >
+<table style = "margin-top: 2vw" >
+    <tr >
+        <td ><a href = "pagemodifier-ajouter.html" > +</a ></td >
+        <th style = "text-align: left" > Ajouter un capteur </th >
+    </tr >
+</table >
+';
+}
+elseif ($_GET['capteur']== 'mic') {
+    echo
+    '<table >
+    <caption > Micro </caption >
+    <tr >
+        <th >
+    IdCapteur:
+        </th >
+        <th >
+    Admin:
+        </th >
+        <th >
+            <div class="onoffswitch" >
+                <input type = "checkbox" name = "onoffswitch" class="onoffswitch-checkbox" id = "myonoffswitch" checked >
+                <label class="onoffswitch-label" for="myonoffswitch" >
+                    <span class="onoffswitch-inner" ></span >
+                    <span class="onoffswitch-switch" ></span >
+                </label >
+            </div >
+
+        </th >
+        <td >
+            <a href = "pagemodifier-ajouter.html" > Modifier</a >
+        </td >
+    </tr >
+    <tr >
+        <th >
+    Place:
+        </th >
+        <th >
+    Etat:
+        </th >
+
+    </tr >
+</table >
+<table style = "margin-top: 2vw" >
+    <tr >
+        <td ><a href = "pagemodifier-ajouter.html" > +</a ></td >
+        <th style = "text-align: left" > Ajouter un capteur </th >
+    </tr >
+</table >
+';
+}
+elseif ($_GET['capteur']== 'vid') {
+    echo
+    '<table >
+    <caption > Video-camera </caption >
+    <tr >
+        <th >
+    IdCapteur:
+        </th >
+        <th >
+    Admin:
+        </th >
+        <th >
+            <div class="onoffswitch" >
+                <input type = "checkbox" name = "onoffswitch" class="onoffswitch-checkbox" id = "myonoffswitch" checked >
+                <label class="onoffswitch-label" for="myonoffswitch" >
+                    <span class="onoffswitch-inner" ></span >
+                    <span class="onoffswitch-switch" ></span >
+                </label >
+            </div >
+
+        </th >
+        <td >
+            <a href = "pagemodifier-ajouter.html" > Modifier</a >
+        </td >
+    </tr >
+    <tr >
+        <th >
+    Place:
+        </th >
+        <th >
+    Etat:
+        </th >
+
+    </tr >
+</table >
+<table style = "margin-top: 2vw" >
+    <tr >
+        <td ><a href = "pagemodifier-ajouter.html" > +</a ></td >
+        <th style = "text-align: left" > Ajouter un capteur </th >
+    </tr >
+</table >
+';
+}
+elseif ($_GET['capteur']== 'temp') {
+    echo
+    '<table >
+    <caption > Temperature </caption >
+    <tr >
+        <th >
+    IdCapteur:
+        </th >
+        <th >
+    Admin:
+        </th >
+        <th >
+            <div class="onoffswitch" >
+                <input type = "checkbox" name = "onoffswitch" class="onoffswitch-checkbox" id = "myonoffswitch" checked >
+                <label class="onoffswitch-label" for="myonoffswitch" >
+                    <span class="onoffswitch-inner" ></span >
+                    <span class="onoffswitch-switch" ></span >
+                </label >
+            </div >
+
+        </th >
+        <td >
+            <a href = "pagemodifier-ajouter.html" > Modifier</a >
+        </td >
+    </tr >
+    <tr >
+        <th >
+    Place:
+        </th >
+        <th >
+    Etat:
+        </th >
+
+    </tr >
+</table >
+<table style = "margin-top: 2vw" >
+    <tr >
+        <td ><a href = "pagemodifier-ajouter.html" > +</a ></td >
+        <th style = "text-align: left" > Ajouter un capteur </th >
+    </tr >
+</table >
+';
+}
+
+?>
+<?php include ("footer.html");
+?>
 </body>
