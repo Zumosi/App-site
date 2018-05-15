9abfc73fc149f67e71dc79896ed8f2174b9e3a86
ValentinPICONE <val.picone@sfr.fr>
2018-05-15 11:00:10 +0200

    header resp

--- Vue/header.php
+++ Vue/header.php
@@ -7,22 +7,22 @@
 <header>
         <div id="header_font"><p> </p>
         </div>
-        <a href="index.php?cible=style"><img src="Vue/image/AtHomeTransparent.png" id="logo"/></a>
+        <a href="index.php?cible=style" ><img src="Vue/image/AtHomeTransparent.png" id="logo"/></a>
             <ul id="menu">
-                <li><a href="index.php?cible=plan">Ma Maison</a>
+                <li><p><a href="index.php?cible=plan">Ma Maison</a></p>
                     <ul>
                         <li><a href="#">Plan</a></li>
                         <li><a href="index.php?cible=page 16">capteurs</a></li>
                         <li><a href="site app julien/statistics/statistics.php">statistiques</a></li>
                     </ul>
                 </li>
-                <li><a href="index.php?cible=shop">Magasin</a>
+                <li><p><a href="index.php?cible=shop">Magasin</a></p>
                     <ul>
                         <li><a href="index.php?cible=shop">Capteurs</a></li>
                         <li><a href="#">factures</a></li>
                     </ul>
                 </li>
-                <li><a href="index.php?cible=myaccount">Mon Compte</a>
+                <li><p><a href="index.php?cible=myaccount">Mon Compte</a></p>
                     <ul>
                         <li><a href="index.php?cible=profil">profil</a></li>
                         <li><a href="#">Gestion capteurs</a></li>
@@ -30,7 +30,7 @@
                         <li><a href="#">Deconnexion</a></li>
                     </ul>
                 </li>
-                <li><a href="#">Contactez Nous</a>
+                <li><p><a href="#">Contactez Nous</a></p>
                     <ul>
                         <li><a href="index.php?cible=forum">Forum</a></li>
                         <li><a href="index.php?cible=page13">FAQ</a></li>
