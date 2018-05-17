efaaa9c2c77fec8917070c53b17f804314f61626
ValentinPICONE <val.picone@sfr.fr>
2018-05-17 10:57:36 +0200

    style

--- Vue/style.php
+++ Vue/style.php
@@ -12,26 +12,16 @@
 
 
 <img src="Vue/image/maisonhead.jpg" title="Maison" id="Maison"/>
-<br>
-<br>
-<p id="presentation"> AtHome est une compagnie qui vous aides à créer ou développer votre propre maison connectée  </p>
-<br>
-<br>
+
 <div id="home">
+    <p id="presentation"> AtHome est une compagnie qui vous aides à créer ou développer votre propre maison connectée  </p>
     <i class="fa fa-home"> Votre Maison</i>
-
-
-</div>
-<br>
-<br>
-<div id="world">
+    <br>
     <i class="fa fa-globe"> Votre Univers</i>
 </div>
-<br>
-<br>
 
 
-<img src="Vue/image/banner11.png" title="banner" id="banner"/>
+<img src="Vue/image/bar.gif" title="banner" id="banner"/>
 
 
 </body>
