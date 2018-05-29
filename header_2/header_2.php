1b31ecc55c15d8baf8b94a06c0e35076d46fd153
cleman95 <34369901+cleman95@users.noreply.github.com>
2018-05-29 12:01:14 +0200

    Add files via upload

---
+++ header_2/header_2.php
@@ -0,0 +1,40 @@
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Responsive Sticky Navbar</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+</head>
+<body>
+<div class="wrapper">
+    <header>
+
+        <nav>
+
+            <div class="menu-icon">
+                <i class="fa fa-bars fa-2x"></i>
+            </div>
+
+            <div class="logo">
+                <img src="AtHomeTransparent.png">
+            </div>
+
+            <div class="menu">
+                <ul>
+                    <li><a href="">Ma Maison</a></li>
+                    <li><a href="#">Magasin</a></li>
+                    <li><a href="#">Mon Compte</a></li>
+                    <li><a href="#">Contactez nous</a></li>
+                </ul>
+            </div>
+        </nav>
+
+    </header>
+
+    <script type="text/javascript" src="menu.js">
+
+
+    </script>
