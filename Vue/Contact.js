938aef040eae899e6256534080182a92acceec7a
Cherub <arnold.neuman@gmail.com>
2018-05-31 11:11:31 +0200

    ajouts des boutons faq + contact

--- Vue/Contact.js
+++ Vue/Contact.js
@@ -1,35 +1,47 @@
 console.log("coucou");
-$('#project1').onclick(
-    function () {
-        var src = $(this).data('onclick');
-        $(this).attr('src', src);
-        $('#question1').html("Numéro de téléphone : 06 666 666 666");
-    },
-    function () {
-        var src = $(this).data('src');
-        $(this).attr('src', src);
+
+function changeImage1() {
+
+    if (document.getElementById("project1").src.indexOf("Vue/image/Minus.png") != -1)
+    {
+        document.getElementById('project1').src = "Vue/image/Plus.jpg";
+        $('#question1').html("Numéro de téléphone : 07 82 66 80 53");
+    }
+    else
+    {
+        document.getElementById("project1").src = "Vue/image/Minus.png";
         $('#question1').html("Numéro de téléphone");
-    });
-$('#project2').onclick(
-    function () {
-        var src = $(this).data('onclick');
-        $(this).attr('src', src);
-        $('#question2').html("Adresse Email : 666 666@ 66666666.666");
-    },
-    function () {
-        var src = $(this).data('src');
-        $(this).attr('src', src);
+    }
+}
+
+function changeImage2() {
+
+    if (document.getElementById("project2").src.indexOf("Vue/image/Minus.png") != -1)
+    {
+        document.getElementById('project2').src = "Vue/image/Plus.jpg";
+        $('#question2').html("Adresse Email : Athome@gmail.com");
+    }
+    else
+    {
+        document.getElementById("project2").src = "Vue/image/Minus.png";
         $('#question2').html("Adresse Email");
-    }),
-$('#project3').onclick(
-    function () {
-        var src = $(this).data('onclick');
-        $(this).attr('src', src);
+    }
+}
+
+function changeImage3() {
+
+    if (document.getElementById("project3").src.indexOf("Vue/image/Minus.png") != -1)
+    {
+        document.getElementById('project3').src = "Vue/image/Plus.jpg";
         $('#question3').html("Adresse Postale :  666 Rue de la 6666");
-        },
-    function () {
-        var src = $(this).data('src');
-        $(this).attr('src', src);
+    }
+
+    else
+    {
+        document.getElementById("project3").src = "Vue/image/Minus.png";
         $('#question3').html("Adresse Postale");
     }
-);
+}
+
+
+
--- Vue/FAQ.php
+++ Vue/FAQ.php
@@ -1,23 +1,23 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>P 13</title>
+        <title>FAQ</title>
         <link rel="stylesheet" type="text/css" href="Vue/FAQ.css">
     </head>
     <body>
 	<h1>Foire aux Questions</h1>
 	<br><br><br>
-	<img id="project1" src="Vue/image/Minus.png" data-src="Vue/image/Minus.png" data-hover="Vue/image/Plus.jpg" />
+	<img id="project1" src="Vue/image/Minus.png" width="50" height="50" onclick="changeImage1();" alt=""  />
     <br>
 	<div id="question1">Question 1</div>
-    <img id="line" src="Vue/image/line2.png"  />
+    <img class="line" src="Vue/image/line2.png" width="1500" height="150" alt=""  />
 	<br><br><br>
-	<img id="project2" src="Vue/image/Minus.png"  data-src="Vue/image/Minus.png" data-hover="Vue/image/Plus.jpg"  />
+	<img id="project2" src="Vue/image/Minus.png" width="50" height="50" onclick="changeImage2();" alt=""  />
     <br>
-    <div id="question2">Question 2</div>
-    <img id="line" src="Vue/image/line2.png" alt=""  />
-    <br><br><br>
+    <div id="question2">Question 2</p></div>
+    <img class="line" src="Vue/image/line2.png" width="1500" height="150" alt=""  />
 	<script src="Vue/jquery-3.3.1.min.js"></script>
-	<script src="Vue/FAQ.js"></script>
+    <script src="Vue/FAQ.js"></script>
+
 	</body>
 </html>
