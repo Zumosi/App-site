4fab21584c899f57e434b98a4fb49686d6b14430
Cherub <arnold.neuman@gmail.com>
2018-05-31 16:33:16 +0200

    Graphique capteur

--- Vue/Graphique.js
+++ Vue/Graphique.js
@@ -1,24 +1,17 @@
-function Courbe() {
-    var myChart = document.getElementById("myChart").getContext("2d");
-    var CaptorChart = new Chart(myChart, {
-        type: "line",
-        data: {
-            labels: ["Capteur"],
-            datasets: [{
-                label: "Consommation",
-                data: [
-                    1,
-                    2,
-                    3,
-                    4,
-                    5,
-                    6,
-                ],
-                backgroundColor: "Red"
-            }]
-        },
-        options: {}
-    });
-}
-Courbe();
-;
+var myChart = document.getElementById("myChart").getContext('2d');
+var CaptorChart = new Chart(myChart, {
+    type: "pie",
+    data: {
+        labels: ["Salle à manger", "Salon", "SdB"],
+        datasets: [{
+            label: "Consommation",
+            data: capteur,
+            backgroundColor: [
+                "Red",
+                "Green",
+                "Blue"
+            ]
+        }]
+    },
+    options: {}
+});
--- Vue/Graphique.php
+++ Vue/Graphique.php
@@ -13,105 +13,73 @@
 
 <?php
 
+include("BDD.php");
 
-function consopuissanceChambre($im)
+function consopuissanceWC()
 {
-    $bdd= new PDO('mysql:host=localhost;dbname=athome;charset=utf8',"root","");
-    $conso=$bdd->prepare('SELECT consommation_value FROM consommation_jour WHERE piece_name="chambre"');
-    $conso->execute(array($im));
-    while($donnees = $conso->fetch()) {
-        echo ' <strong class="consoValue">' . $donnees['consommation_value'] . '</strong><br/>';
-    }
-    $power=$bdd->prepare('SELECT puissance_value FROM puissance_jour WHERE piece_name="chambre"');
-    $power->execute(array($im));
-    while($donnees2 = $power->fetch()) {
-        echo ' <strong class="powerValue">' . $donnees2['puissance_value'] . '</strong><br/>';
-    }
+    $server = "localhost";
+    $login = "root";
+    $pass = "";
+    $connexion = new PDO("mysql:host=$server;dbname=athome", $login, $pass);
+    $requete = $connexion->prepare('SELECT consommation_value FROM consommation_jour WHERE piece_name="wc" LIMIT 1');
+    $requete->execute();
+   $conso = $requete->fetchAll();
+  $value=$conso[0]["consommation_value"];
+  return $value;
+
+
 }
 
-
-function consopuissanceWC($im)
-{
-    $bdd= new PDO('mysql:host=localhost;dbname=athome;charset=utf8',"root","");
-    $conso=$bdd->prepare('SELECT * FROM consommation_jour WHERE piece_name="wc"');
-    $conso->execute(array($im));
-    while($donnees = $conso->fetch()) {
-        echo '<strong class="consoValue">' . $donnees['consommation_value'] . '</strong><br/>';
-    }
-    $power=$bdd->prepare('SELECT puissance_value FROM puissance_jour WHERE piece_name="wc"');
-    $power->execute(array($im));
-    while($donnees2 = $power->fetch()) {
-        echo ' <strong class="powerValue">' . $donnees2['puissance_value'] . '</strong><br/>';
-    }
-}
-
-//echo 'puissance : <strong>' . $puissance . '</strong><br/>';
-
-function consopuissanceSalon($im)
-{
-    $bdd= new PDO('mysql:host=localhost;dbname=athome;charset=utf8',"root","");
-  /*  $conso=$bdd->prepare('SELECT * FROM consommation_jour WHERE piece_name="salon"');
-    $conso->execute(array($im));
-    while($donnees = $conso->fetch()) {
-        echo ' <strong class="consoValue">' . $donnees['consommation_value'] . '</strong><br/>';
-        echo 'Vous avez consommé' .($_POST['puissance_value']) . '!';
-    }*/
-    $power=$bdd->prepare('SELECT puissance_value FROM puissance_jour WHERE piece_name="salon"');
-   $power->execute(array($im));
-    while($donnees2 = $power->fetch()) {
-        echo ' <strong class="powerValue">' . $donnees2['puissance_value'] . '</strong><br/>';
-        echo 'Vous avez consommé' .($_POST['puissance_value']) . '!';
-    }
-}
-
-//echo 'puissance : <strong>' . $puissance . '</strong><br/>';
-
-function consopuissanceCuisine($im)
-{
-    $bdd= new PDO('mysql:host=localhost;dbname=athome;charset=utf8',"root","");
-    $conso=$bdd->prepare('SELECT * FROM consommation_jour WHERE piece_name="cuisine"');
-    $conso->execute(array($im));
-    while($donnees = $conso->fetch()) {
-        return '<strong class="consoValue">' . $donnees['consommation_value'] . '</strong><br/>';
-    }
-    $power=$bdd->prepare('SELECT puissance_value FROM puissance_jour WHERE piece_name="cuisine"');
-    $power->execute(array($im));
-    while($donnees2 = $power->fetch()) {
-        return $donnees2['puissance_value'];
-    }
-}
-
-//echo 'puissance : <strong>' . $puissance . '</strong><br/>';
-
-function consopuissanceSdb($im)
-{
-  $bdd = new PDO('mysql:host=localhost;dbname=athome;charset=utf8', "root", "");
- /* $conso = $bdd->prepare('SELECT * FROM consommation_jour WHERE piece_name="sdb"');
-  $conso->execute(array($im));
-  while ($donnees = $conso->fetch()) {
-      echo $donnees['consommation_value'];
-  }
-*/
-
-
-
-  $power=$bdd->prepare('SELECT puissance_value FROM puissance_jour WHERE piece_name="sdb"');
-  $power->execute(array($im));
-  while($donnees2 = $power->fetch()) {
-      return $donnees2['puissance_value'];
-  }
-}
-//echo 'puissance : <strong>' . $puissance . '</strong><br/>';
-
-$numberPHP = consopuissanceSalon($im);
-echo $numberPHP;
 ?>
+
+<?php
+
+function consopuissanceChambre()
+{
+    $server = "localhost";
+    $login = "root";
+    $pass = "";
+    $connexion = new PDO("mysql:host=$server;dbname=athome", $login, $pass);
+    $requete = $connexion->prepare('SELECT consommation_value FROM consommation_jour WHERE piece_name="Chambre" LIMIT 1');
+    $requete->execute();
+    $conso = $requete->fetchAll();
+    $value=$conso[0]["consommation_value"];
+    return $value;
+
+
+}
+
+?>
+
+<?php
+
+function consopuissanceSalon()
+{
+    $server = "localhost";
+    $login = "root";
+    $pass = "";
+    $connexion = new PDO("mysql:host=$server;dbname=athome", $login, $pass);
+    $requete = $connexion->prepare('SELECT consommation_value FROM consommation_jour WHERE piece_name="Salon" LIMIT 1');
+    $requete->execute();
+    $conso = $requete->fetchAll();
+    $value=$conso[0]["consommation_value"];
+    return $value;
+}
+
+?>
+
+<?php
+$capteur = array (consopuissanceWC(),consopuissanceChambre(),consopuissanceSalon());
+echo '<script>';
+echo 'var capteur = ' .json_encode($capteur) . ';';
+echo '</script>';
+
+?>
+
 <div class="container">
     <canvas id="myChart"></canvas>
-    <script type="text/javascript" src="Graphique.js"></script>
 </div>
-<script src="FonctionJS.js"></script>
-
+<script src="Courbe.js"></script>
 </body>
 </html>
 
