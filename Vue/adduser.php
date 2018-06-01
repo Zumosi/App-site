ae7684451fa44f290480566286a9cfcb7e67b1f2
Alexandre <alexandrenegre92@hotmail.com>
2018-06-01 16:56:58 +0200

    test gestion

---
+++ Vue/adduser.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>adduser</title>
+</head>
+
+<body>
+
+
+<form method="post" action="cible.php">
+  
+<label> Email de l'utilisateur secondaire: </label> <input id="mail" name="mail" type="text" placeholder="Texte" /> </br>
+
+<input type="submit" value="Envoyer" id="bouton">
+
+
+</form>
+
+</body>
+
+</html>
