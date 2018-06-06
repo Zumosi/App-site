6eea61a73d535204f71e3145f350378fcea19b42
Cherub <arnold.neuman@gmail.com>
2018-06-06 13:29:56 +0200

    1

---
+++ Vue/Courbure.js
@@ -0,0 +1,50 @@
+/*function Salon(){
+    new Chart(document.getElementById("Salon"), {
+        type: 'line',
+        data: {
+            labels: date,
+            datasets: [{
+                data: consoSalon,
+                label: "Consommation",
+                borderColor: "#3e95cd",
+                fill: false
+            }, {
+                data: puissanceSalon,
+                label: "Puissance",
+                borderColor: "#8e5ea2",
+                fill: false
+            }
+            ]
+        },
+        options: {
+            title: {
+                display: true,
+                text: 'Consomation de votre Salon'
+            }
+        }
+    });};*/
+function Chambre(){
+new Chart(document.getElementById("Chambre"), {
+    type: 'line',
+    data: {
+        labels: date,
+        datasets: [{
+            data: consoChambre,
+            label: "Consommation",
+            borderColor: "#3e95cd",
+            fill: false
+        }, {
+            data: puissanceChambre,
+            label: "Puissance",
+            borderColor: "#8e5ea2",
+            fill: false
+        }
+        ]
+    },
+    options: {
+        title: {
+            display: true,
+            text: 'Consomation de votre Chambre'
+        }
+    }
+});};
