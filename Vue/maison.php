891719090eaf8d2c20ba4e861d6b44000fe065d9
ValentinPICONE <val.picone@sfr.fr>
2018-06-08 16:39:12 +0200

    'css'

--- Vue/maison.php
+++ Vue/maison.php
@@ -14,7 +14,7 @@
         <td ><a href=="index.php?cible=plan"><img  src="vue/image/maison.jpg" />Mes habitations</a></td>
     </tr>
     <tr>
-        <td ><a href="index.php?cible=capteurs>"><img  src="vue/image/Capteur.jpg" />Mes capteurs</a></td>
+        <td ><a href="index.php?cible=capteurs"><img  src="vue/image/Capteur.jpg" />Mes capteurs</a></td>
     </tr>
     <tr>
         <td ><a href="index.php?cible=statistics"><img  src="vue/image/stat.png"  id="statist"/>Statistiques</a></td>
