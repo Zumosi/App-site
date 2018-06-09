c58911e58a1ef0b258ace780ac1b0de8e2609c98
Franck <n-franck@hotmail.fr>
2018-06-09 09:56:30 +0200

    fct stock capteur acheter

--- Vue/traitement.php
+++ Vue/traitement.php
@@ -1,7 +1,11 @@
 <?php
+session_start()
+?>
+<?php
 
 if (isset($_POST['addpanier'])) {
-    header("location:../index.php?cible=panier&quantite=".$_POST["quantite"]."&prix=".$_POST["prix"]." ");
+
+    header("location:../index.php?cible=panier&quantite=" . $_POST["quantite"] . "&prix=" . $_POST["prix"] . " ");
 
 }
 
@@ -11,7 +15,6 @@ if (isset($_POST['addpanier'])) {
 
 if (isset($_POST['Validerpanier'])) {
     header("location:../index.php?cible=capteurdispo ");
-
 }
 
 ?>
@@ -28,8 +31,19 @@ if (isset($_POST['send'])) {
 <?php
 
 if (isset($_POST['envoyer'])) {
-    header("location:../index.php?cible=formpieceliste&piece=".$_POST["piece"]." ");
+    header("location:../index.php?cible=formpieceliste&piece=" . $_POST["piece"] . " ");
 
 }
+
+?>
+<?php
+$bdd = new PDO('mysql:host=localhost;dbname=athome;charset=utf8', 'root', '');
+$reponse = $bdd->prepare('INSERT INTO stockuser( id_captacheter,id_acheteur,id_quantite) VALUES (:captacheter, :acheteur, :quantite)');
+$reponse->execute(array(
+    'captacheter' => 1,
+    'acheteur' => $_SESSION['id'],
+    'quantite' =>$_POST["quantite"],
+));
+$reponse->closeCursor();
 
 ?>
